5611170921a306c52d713a3b1d76149ee3b06041
ShihabPalekar <shihabpalekar23@gmail.com>
2020-04-23 15:10:05 +0530

    find-next-square

---
+++ Day4/script.js
@@ -0,0 +1,23 @@
+/* 
+Complete the findNextSquare method that finds the next integral perfect square 
+after the one passed as a parameter.
+
+If the parameter is itself not a perfect square, 
+than -1 should be returned.
+
+findNextSquare(121) --> returns 144
+findNextSquare(625) --> returns 676
+findNextSquare(114) --> returns -1 since 114 is not a perfect
+*/
+
+function findNextSquare(sq) {
+    let sqRoot = Math.sqrt(sq);
+    let nextSq = (sqRoot+1) * (sqRoot+1);
+    let answer;
+    if (sqRoot % 1 == 0) {
+      answer = nextSq;
+    } else if (sqRoot % 1 !== 0) {
+      answer = (-1);
+    }
+    return answer;
+  }
