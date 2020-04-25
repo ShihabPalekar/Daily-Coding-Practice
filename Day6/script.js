e57cc3732091113b1fcc81fd469f5124e34df65b
ShihabPalekar <shihabpalekar23@gmail.com>
2020-04-25 22:08:47 +0530

    NumsInDescendingOrder

---
+++ Day6/script.js
@@ -0,0 +1,13 @@
+/*
+Your task is to make a function that can take any non-negative integer as an argument 
+and return it with its digits in descending order. Essentially, rearrange the digits 
+to create the highest possible number.
+*/
+
+function descendingOrder(n){
+    let arrayOfNum = ("" + n).split("").sort(function(a, b){return b-a}); 
+    let stringOfNum = arrayOfNum.join("");
+    let newStr = parseInt(stringOfNum, 10);
+    return newStr;
+  }
+  
