2d76d31a717a2883cd20c66d07f9b69cb23ccd4e
ShihabPalekar <shihabpalekar23@gmail.com>
2020-04-30 13:38:50 +0530

    Reverse-Words

---
+++ Day11/script.js
@@ -0,0 +1,16 @@
+/*
+Complete the function that accepts a string parameter, 
+and reverses each word in the string. All spaces in the 
+string should be retained.
+
+Examples
+"This is an example!" ==> "sihT si na !elpmaxe"
+"double  spaces"      ==> "elbuod  secaps"
+*/
+
+function reverseWords(str) {
+    let revStr = str.split("").reverse().join("");
+    let ans =   revStr.split(" ").reverse().join(" "); 
+    return ans;
+  }
+  
